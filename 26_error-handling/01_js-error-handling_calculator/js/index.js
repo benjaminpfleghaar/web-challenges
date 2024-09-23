e8430d056ff62e6540846fb39905f5a6960591f6
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-09-23 14:56:01 +0200

    add error-handling

---
+++ 26_error-handling/01_js-error-handling_calculator/js/index.js
@@ -0,0 +1,29 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+
+const operations = {
+	add: (a, b) => a + b,
+	subtract: (a, b) => a - b,
+	multiply: (a, b) => a * b,
+	divide: (a, b) => {
+		try {
+			if (b === 0) {
+				throw new Error("Cannot divide by zero!");
+			}
+			return a / b;
+		} catch (error) {
+			document.querySelector(".error").innerHTML = error;
+			return "...";
+		}
+	},
+};
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const firstNumber = Number(event.target.firstNumber.value);
+	const secondNumber = Number(event.target.secondNumber.value);
+	const operation = event.target.operation.value;
+	output.innerText = operations[operation](firstNumber, secondNumber);
+});
