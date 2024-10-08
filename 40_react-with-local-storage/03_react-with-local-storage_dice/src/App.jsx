b2a52bff87a66761cecb8d6949ca153e084a6af7
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-08 14:54:28 +0200

    add react-with-local-storage

---
+++ 40_react-with-local-storage/03_react-with-local-storage_dice/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { D6Button } from "./components/D6Button";
+import useLocalStorageState from "use-local-storage-state";
+import History from "./components/History";
+import "./styles.css";
+
+export default function App() {
+	const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+	return (
+		<div className="app">
+			<main className="app__main">
+				<D6Button rolls={rolls} setRolls={setRolls} />
+			</main>
+			<aside className="app__aside">
+				<History rolls={rolls} />
+			</aside>
+		</div>
+	);
+}
