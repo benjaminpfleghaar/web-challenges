2257b0bf7ebdd7c279af65fa78c74f827d232a34
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-08 11:51:04 +0200

    refactor unit test

--- 39_js-unit-testing/01_js-unit-testing_get-animal/index.js
+++ 39_js-unit-testing/01_js-unit-testing_get-animal/index.js
@@ -1,5 +1,5 @@
 export function getAnimal(animal) {
   if (animal === "cats") return "I totally love cats!";
-  if (animal) return "I like dogs!";
+  if (animal) return "I like " + animal + "!";
   if (animal === undefined) return "I do not like animals at all!";
 }
