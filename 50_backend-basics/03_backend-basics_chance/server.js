30a6baa9ca6aafdc67c2f459ae02b71e1511d7a8
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-22 16:49:57 +0200

    refactor Chance()

--- 50_backend-basics/03_backend-basics_chance/server.js
+++ 50_backend-basics/03_backend-basics_chance/server.js
@@ -1,12 +1,12 @@
 import Chance from "chance";
 import { createServer } from "node:http";
 
-const chance = new Chance();
-const name = chance.name();
-const age = chance.age();
-const profession = chance.profession({ rank: true });
-
 export const server = createServer((reuqest, response) => {
+	const chance = new Chance();
+	const name = chance.name();
+	const age = chance.age();
+	const profession = chance.profession({ rank: true });
+
 	response.statusCode = 200;
 	response.end(`Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`);
 });
