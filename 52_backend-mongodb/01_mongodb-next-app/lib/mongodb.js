d5f3c77ea9ee5b27c5a71d643389094562e643e7
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-23 16:50:55 +0200

    refactor mongodb.js

--- 52_backend-mongodb/01_mongodb-next-app/lib/mongodb.js
+++ 52_backend-mongodb/01_mongodb-next-app/lib/mongodb.js
@@ -1,29 +1,29 @@
-// mongodb.js
-
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-const options = {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-};
+if (!process.env.MONGODB_URI) {
+	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
 
-let client;
+const uri = process.env.MONGODB_URI;
+const options = {};
+
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-	throw new Error("Add Mongo URI to .env.local");
+if (process.env.NODE_ENV === "development") {
+	// In development mode, use a global variable so that the value
+	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	const globalWithMongo = global;
+
+	if (!globalWithMongo._mongoClient) {
+		globalWithMongo._mongoClient = new MongoClient(uri, options);
+	}
+	clientPromise = globalWithMongo._mongoClient;
+} else {
+	// In production mode, it's best to not use a global variable.
+	clientPromise = new MongoClient(uri, options);
 }
 
-if (process.env.NODE_ENV === "development") {
-	if (!global._mongoClientPromise) {
-		client = new MongoClient(uri, options);
-		global._mongoClientPromise = client.connect();
-	}
-	clientPromise = global._mongoClientPromise;
-} else {
-	client = new MongoClient(uri, options);
-	clientPromise = client.connect();
-}
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
 
 export default clientPromise;
