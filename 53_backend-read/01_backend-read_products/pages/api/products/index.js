b64c01bdd67cb8db87a790fd740a96942a0191d1
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-24 12:24:25 +0200

    add backend-read

---
+++ 53_backend-read/01_backend-read_products/pages/api/products/index.js
@@ -0,0 +1,13 @@
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+	await dbConnect();
+
+	if (request.method === "GET") {
+		const products = await Product.find();
+		response.status(200).json(products);
+	} else {
+		response.status(405).json({ status: "Method not allowed" });
+	}
+}
