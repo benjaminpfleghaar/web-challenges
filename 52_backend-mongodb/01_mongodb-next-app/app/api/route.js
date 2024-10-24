322cd422a02f336bb07d2f10a216c0f2b031fa34
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-24 09:32:21 +0200

    refactor mongodb.js

--- 52_backend-mongodb/01_mongodb-next-app/app/api/route.js
+++ 52_backend-mongodb/01_mongodb-next-app/app/api/route.js
@@ -1,9 +1,8 @@
-import clientPromise from "@/lib/mongodb";
+import client from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
-		const client = await clientPromise;
 		const db = client.db("fish-shop-database");
 		const products = await db.collection("products").find({}).toArray();
 
